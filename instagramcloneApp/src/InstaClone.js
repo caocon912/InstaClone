380f510a6cdf5af67b63ded2019947d92648485c
DESKTOP-6P4KML7\dotha <dothanhthi1209@gmail.com>
2019-04-23 16:03:47 +0700

    demo 1

---
+++ instagramcloneApp/src/InstaClone.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {View,Text,StyleSheet, Image} from 'react-native';
+
+
+export default class InstaClone extends Component{
+    render(){
+        return(
+            <View>
+                <Text>My Instagram Clone </Text>
+            </View>
+
+        )
+    }
+}
