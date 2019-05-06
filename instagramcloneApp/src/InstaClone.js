df08f49638707f4086365073a10b6c14dc6767e3
DESKTOP-6P4KML7\dotha <dothanhthi1209@gmail.com>
2019-05-06 12:32:49 +0700

    login va register

--- instagramcloneApp/src/InstaClone.js
+++ instagramcloneApp/src/InstaClone.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import {View,Text,StyleSheet, Image} from 'react-native';
-//import {MainFeed, Login} from './components/screens';
+import {View,Text,StyleSheet, Image,Navigator} from 'react-native';
+//import {Navigator} from 'react-navigation'
+import Login from '../screens/Login';
+import Register from '../screens/Register';
 
 
 
 export default class InstaClone extends Component{
+    renderSence(route,navigator){
+        switch(route.name){
+            case 'login': return(
+                <Login 
+                goRegister={()=>{navigator.push({name:'register'})}}
+                />
+            )
+            case 'register':return(
+                <Register 
+                goLogin={()=>navigator.push({name:'login'})}
+                />
+            )
+        }
+    }
     render(){
         return(
-            <Text>Hello</Text>
-        )
+            <Navigator
+                initialRoute={{name:Login}}
+                renderSence={this.renderSence}>
+
+
+            </Navigator>
+        );
     }
 }
