0e090c00f72f5ffe9f8f7a096451434e73db05b3
TranYenNhi <nhitty099@gmail.com>
2019-05-13 22:09:15 +0700

    Update index.js

--- instagramcloneApp/index.js
+++ instagramcloneApp/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 //import App from './App';
-import App from './src/components/screens/InstaClone'
+import App from './src/components/screens/MainScreen'
 AppRegistry.registerComponent('instagramcloneApp', () =>App);
